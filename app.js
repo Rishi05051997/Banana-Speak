c9be419a1bb7346a29e8d2881bd9e1424cd3b4fb
vrushabhdhatrak <vrushabhdhatral10@gmail.com>
2021-03-17 17:55:01 +0530

    taking input & console

--- app.js
+++ app.js
@@ -9,3 +9,17 @@
 
 // // output
 // alert(welcomeMsg);
+
+var buttonClick = document.querySelector('#btn-translate');
+
+console.log(buttonClick);
+
+buttonClick.addEventListener("click", function clickEventHandler(){
+    console.log('Clicked...!!!!')
+    console.log('Input '+ txtInput.value);
+})
+
+//////
+var txtInput = document.querySelector('#txt-Input');
+
+console.log(txtInput);
